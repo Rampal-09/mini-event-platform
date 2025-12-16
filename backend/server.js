628072cbb4a26d2892c77d535f557d808bbef343
Rampal-09 <rampaltanwar0929@gmail.com>
2025-12-16 13:22:31 +0530

    Initial MERN project setup with Vite frontend and Express backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server running on  http://localhost port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
